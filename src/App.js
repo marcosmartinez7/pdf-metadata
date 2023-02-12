1091bc378a056348a177b4b66abbf85f97407e85
Marcos Martínez <mmartinezciompi@gmail.com>
2023-02-12 16:28:51 -0300

    Download as zip

--- src/App.js
+++ src/App.js
@@ -2,73 +2,146 @@ import "./App.css";
 import { PDFDocument, PDFName, PDFString } from "pdf-lib";
 import { useState } from "react";
 import download from "downloadjs";
+import JSZip from "jszip";
+
 function App() {
-    const [file, setFile] = useState(null);
+    const [files, setFiles] = useState([]);
+    const [principalInvestigators, setPrincipalInvestigators] = useState("");
+    const [secondaryInvestigators, setSecondaryInvestigators] = useState("");
+    const [clinicalSite, setClinicalSite] = useState("");
+    const [patientCode, setPatientCode] = useState("");
+    const [zipName, setZipName] = useState("");
 
     const handleFileInput = (event) => {
-        const file = event.target.files[0];
-        setFile(file);
+        setFiles(Array.from(event.target.files));
     };
 
-    const handleExtract = async () => {
-        if (!file) {
-            console.log("Please select a file first");
+    const setMetadata = async () => {
+        return await Promise.all(
+            files.map(async (file) => {
+                const fileReader = new FileReader();
+                fileReader.readAsArrayBuffer(file);
+
+                return new Promise((resolve) => {
+                    fileReader.onload = async () => {
+                        const typedArray = new Uint8Array(fileReader.result);
+                        // Open a PDF document from the Uint8Array
+                        const pdfDoc = await PDFDocument.load(typedArray);
+
+                        pdfDoc
+                            .getInfoDict()
+                            .set(
+                                PDFName.of("Principal Investigators"),
+                                PDFString.of(principalInvestigators)
+                            );
+                        pdfDoc
+                            .getInfoDict()
+                            .set(
+                                PDFName.of("Secondary Investigators"),
+                                PDFString.of(secondaryInvestigators)
+                            );
+                        pdfDoc
+                            .getInfoDict()
+                            .set(
+                                PDFName.of("Clinical Site"),
+                                PDFString.of(clinicalSite)
+                            );
+                        pdfDoc
+                            .getInfoDict()
+                            .set(
+                                PDFName.of("Patient Code"),
+                                PDFString.of(patientCode)
+                            );
+
+                        const pdfBytes = await pdfDoc.save();
+                        resolve({ name: file.name, data: pdfBytes });
+                    };
+                });
+            })
+        );
+    };
+
+    const handleSaveAsZip = async () => {
+        if (!files.length) {
+            alert("Please select one or more files first");
             return;
         }
-        const fileReader = new FileReader();
-        fileReader.onload = async () => {
-            const typedArray = new Uint8Array(fileReader.result);
-
-            // Open a PDF document from the Uint8Array
-            const pdfDoc = await PDFDocument.load(typedArray);
-
-            console.dir(pdfDoc);
-            console.log(pdfDoc.getTitle());
-            console.log(pdfDoc.getInfoDict());
-
-            console.log(pdfDoc.getKeywords());
-            console.log(
-                "Custom property",
-                pdfDoc.getInfoDict().get(PDFName.of("Custom"))
-            );
-
-            console.log(
-                "Clinical site",
-                pdfDoc.getInfoDict().get(PDFName.of("Clinical site"))
-            );
-            console.log("Changing keywords");
-
-            const oldKeywords = pdfDoc.getKeywords();
-            pdfDoc.setKeywords(["Jhon", "Patricia"].concat(oldKeywords));
-
-            console.log(pdfDoc.getKeywords());
-
-            console.log("Adding new property");
-            pdfDoc
-                .getInfoDict()
-                .set(PDFName.of("Clinical site"), PDFString.of("Hospital"));
-            console.log(
-                "Clinical site",
-                pdfDoc.getInfoDict().get(PDFName.of("Clinical site"))
-            );
-
-            const pdfBytes = await pdfDoc.save();
-
-            // Trigger the browser to download the PDF document
-            download(
-                pdfBytes,
-                "pdf-lib_form_creation_example.pdf",
-                "application/pdf"
-            );
-        };
-        fileReader.readAsArrayBuffer(file);
+        if (!zipName) {
+            alert("Please select a zip name first");
+            return;
+        }
+        const updatedFiles = await setMetadata();
+        console.log("updated files", updatedFiles);
+        const zip = new JSZip();
+        updatedFiles.forEach(({ name, data }) => {
+            zip.file(name, data);
+        });
+        const content = await zip.generateAsync({ type: "blob" });
+        download(content, "pdf-files.zip", "application/zip");
     };
 
     return (
-        <div>
-            <input type="file" onChange={handleFileInput} />
-            <button onClick={handleExtract}>Extract Metadata</button>
-        </div>
+        <form>
+            <div>
+                <label htmlFor="principalInvestigators">
+                    Principal Investigators:
+                </label>
+                <textarea
+                    cols="50"
+                    rows={20}
+                    type="text"
+                    id="principalInvestigators"
+                    value={principalInvestigators}
+                    onChange={(e) => setPrincipalInvestigators(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="secondaryInvestigators">
+                    Secondary Investigators:
+                </label>
+                <textarea
+                    cols="50"
+                    rows={20}
+                    type="text"
+                    id="secondaryInvestigators"
+                    value={secondaryInvestigators}
+                    onChange={(e) => setSecondaryInvestigators(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="clinicalSite">Clinical Site:</label>
+                <input
+                    type="text"
+                    id="clinicalSite"
+                    value={clinicalSite}
+                    onChange={(e) => setClinicalSite(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="patientCode">Patient Code:</label>
+                <input
+                    type="text"
+                    id="patientCode"
+                    value={patientCode}
+                    onChange={(e) => setPatientCode(e.target.value)}
+                />
+            </div>
+            <div>
+                <input type="file" onChange={handleFileInput} multiple />
+            </div>
+            <div>
+                <label htmlFor="zipName">Zip name:</label>
+                <input
+                    type="text"
+                    id="zipName"
+                    value={zipName}
+                    onChange={(e) => setZipName(e.target.value)}
+                />
+            </div>
+            <button type="button" onClick={handleSaveAsZip}>
+                Store metadata
+            </button>
+        </form>
     );
 }
 
